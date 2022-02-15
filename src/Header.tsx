39f37f7cd81b33bdd47c50d289a5806675ae8c60
alextsapin <alextsapin@yandex.ru>
2022-02-15 18:55:59 +0200

    Edit app

---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">News Feed</a></li>
+                <li><a href="#">Messages</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header
